f697dfbc4efd4c6402d1daa7e826245d41a60898
LynxesMCPE <strikecounter27@gmail.com>
2017-05-28 15:53:18 +0800

    Updated main.php

--- src/HeadDropper/main.php
+++ src/HeadDropper/main.php
@@ -12,13 +12,16 @@ use pocketmine\math\Vector3;
 class Main extends PluginBase implements Listener {
 	
 	
-	public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
-	
-	public function onDeath(PlayerDeathEvent $event) {
-		$player = $event->getPlayer();
-		$player->getLevel()->dropItem(new Vector3($player->getX(), $player->getY(), $player->getZ()), Item::get(Item::SKULL, 3, 1));
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+    
+    public function onDeath(PlayerDeathEvent $event){
+        $event->setDrops(Item::Get(Item::SKULL:3, 0, 1));
+        }
+}
+    public function onDisable(){
+    }
 		$player->sendMessage("You received a head!");
 	}
 }
